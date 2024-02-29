a305ef8fccaa79ee683dbe4ddee1c622bb94ecbd
peterartman <peter.artman@gmail.com>
2024-02-29 17:22:58 +0000

    Added colour scheme in colors.xml.

---
+++ app/src/main/java/com/finalyearproject/soundsoul/auth/SpotifyImplicitLoginActivityImpl.kt
@@ -0,0 +1,29 @@
+package com.finalyearproject.soundsoul.auth
+
+import android.content.Intent
+import com.adamratzman.spotify.SpotifyImplicitGrantApi
+import com.adamratzman.spotify.SpotifyScope
+import com.adamratzman.spotify.auth.implicit.AbstractSpotifyAppImplicitLoginActivity
+import com.adamratzman.spotifyandroidexample.BuildConfig
+import com.adamratzman.spotifyandroidexample.SpotifyPlaygroundApplication
+import com.adamratzman.spotifyandroidexample.activities.ActionHomeActivity
+import com.adamratzman.spotifyandroidexample.toast
+
+class SpotifyImplicitLoginActivityImpl : AbstractSpotifyAppImplicitLoginActivity() {
+    override val state: Int = 1337
+    override val clientId: String = BuildConfig.SPOTIFY_CLIENT_ID
+    override val redirectUri: String = BuildConfig.SPOTIFY_REDIRECT_URI_AUTH
+    override val useDefaultRedirectHandler: Boolean = false
+    override fun getRequestingScopes(): List<SpotifyScope> = SpotifyScope.values().toList()
+
+    override fun onSuccess(spotifyApi: SpotifyImplicitGrantApi) {
+        val model = (application as SpotifyPlaygroundApplication).model
+        model.credentialStore.setSpotifyApi(spotifyApi)
+        toast("Authentication via spotify-auth has completed. Launching TrackViewActivity..")
+        startActivity(Intent(this, ActionHomeActivity::class.java))
+    }
+
+    override fun onFailure(errorMessage: String) {
+        toast("Auth failed: $errorMessage")
+    }
+}
---
+++ app/src/main/java/com/finalyearproject/soundsoul/auth/SpotifyPkceLoginActivityImpl.java
@@ -0,0 +1,6 @@
+package com.finalyearproject.soundsoul.auth;
+
+import android.app.Activity;
+
+public class SpotifyPkceLoginActivityImpl extends Activity {
+}
